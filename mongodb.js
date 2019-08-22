6d79c53d4e887fbe756318bddccefabb3d935bf5
Downey <GYMJN0@usa.dce.usps.gov>
2019-08-22 09:31:57 -0500

    Removed unused environment variables

--- mongodb.js
+++ mongodb.js
@@ -1,19 +1,19 @@
-const {
-  MongoClient,
-  ObjectID
-} = require("mongodb")
+// const {
+//   MongoClient,
+//   ObjectID
+// } = require("mongodb")
 
-const connectionURL = process.env.DB_CONNECTION_URL
-const databaseName = process.env.DB_NAME
+// const connectionURL = process.env.DB_CONNECTION_URL
+// const databaseName = process.env.DB_NAME
 
-MongoClient.connect(
-  connectionURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  },
-  (error, client) => {
-    const db = error ?
-      console.log("Unable to connect to database!") :
-      client.db(databaseName)
-  }
-)
+// MongoClient.connect(
+//   connectionURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   },
+//   (error, client) => {
+//     const db = error ?
+//       console.log("Unable to connect to database!") :
+//       client.db(databaseName)
+//   }
+// )
